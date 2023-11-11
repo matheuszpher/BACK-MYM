d0b875926415464bdcb519d3bd26c146926bd8b1
Natan <zerogamerptbr@gmail.com>
2023-11-11 01:12:15 -0300

    :books::docs:Add Swagger documentation and JSON parser middleware to server

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
 import "reflect-metadata";
 import * as dotenv from "dotenv";
 import express from "express";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import swaggerUi from "swagger-ui-express";
 
 // eslint-disable-next-line import/no-extraneous-dependencies
-
 import "./database";
 
 import "./shared/container";
+import swaggerFile from "./swagger.json";
 
 const app = express();
+app.use(express.json());
 dotenv.config();
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(3333, () => console.log("Server is Running on port 3333!"));
