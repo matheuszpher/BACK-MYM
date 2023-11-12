5ad3fbe5db9fee79510c1fbe2f71303b2dea2fbd
Natan <zerogamerptbr@gmail.com>
2023-11-12 15:00:59 -0300

    :construction:build:ProfessorRepository to container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,6 +1,12 @@
 import { container } from "tsyringe";
 
+import { ProfessorRepository } from "../../modules/users/repositories/implementantions/ProfessorRepository";
 import { UserRepository } from "../../modules/users/repositories/implementantions/UserRepository";
+import { IProfessorRepository } from "../../modules/users/repositories/IProfessorRepository";
 import { IUserRepository } from "../../modules/users/repositories/IUserRepository";
 
 container.registerSingleton<IUserRepository>("UserRepository", UserRepository);
+container.registerSingleton<IProfessorRepository>(
+    "ProfessorRepository",
+    ProfessorRepository,
+);
