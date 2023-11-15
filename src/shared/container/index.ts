95eb901151b2b221b413c962a40e4de36befbbed
Natan <zerogamerptbr@gmail.com>
2023-11-15 10:41:25 -0300

    Add MonitorRepository to container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,7 @@
 import { container } from "tsyringe";
 
+import { IMonitorRepository } from "../../modules/users/repositories/IMonitorRepository";
+import { MonitorRepository } from "../../modules/users/repositories/implementantions/MonitorRepository";
 import { ProfessorRepository } from "../../modules/users/repositories/implementantions/ProfessorRepository";
 import { UserRepository } from "../../modules/users/repositories/implementantions/UserRepository";
 import { IProfessorRepository } from "../../modules/users/repositories/IProfessorRepository";
@@ -9,4 +11,8 @@ container.registerSingleton<IUserRepository>("UserRepository", UserRepository);
 container.registerSingleton<IProfessorRepository>(
     "ProfessorRepository",
     ProfessorRepository,
+);
+container.registerSingleton<IMonitorRepository>(
+    "MonitorRepository",
+    MonitorRepository,
 );
