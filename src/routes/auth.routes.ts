0217198a26b5c810cfb9d0ba49924d7423e1643e
Natan <zerogamerptbr@gmail.com>
2023-11-16 09:41:18 -0300

    :sparkles:feat:validating user in a good way :)

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 
 import { RegisterUserController } from "../modules/users/useCases/registerUser/RegisterUserController";
+import { ValidateUserController } from "../modules/users/useCases/validateUser/ValidateUserController";
 
 const authRouter = Router();
 const registerUserController = new RegisterUserController();
 authRouter.post("/register", registerUserController.handle);
+const validateUserController = new ValidateUserController();
+authRouter.post("/validate", validateUserController.handle);
 
 export { authRouter };
